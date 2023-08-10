f702df1e3a0a36a6d066bf0785bf946f8ebba6d7
amirongithubb <139608315+amirongithubb@users.noreply.github.com>
2023-08-10 22:09:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('active');
+  const menuToggle = document.querySelector('.menu-toggle');
+  menuToggle.classList.toggle('active');
+}
